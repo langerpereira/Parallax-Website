1c4a1603e38b2da0f1f7eb5ef4026c4de52f9670
Langer Pereira <langerpereira12@gmail.com>
2024-07-19 22:41:12 +0530

    commit1

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const parallex_el = document.querySelectorAll(".parallex");
+
+let xValue = 0, yValue = 0;
+
+window.addEventListener("mousemove", (e) =>{
+    xValue = e.clientX - window.innerWidth / 2;
+    yValue = e.clientY - window.innerHeight / 2;
+
+    parallex_el.forEach((el) => {
+        el.style.transform = `translateX(calc(-50% + ${xValue}px)) translateY(calc(-50% + ${yValue}px))`;
+    });
+
+});
