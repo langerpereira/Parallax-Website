ee195ccb9217d95a14c4dad18b6786f6d8357fef
Langer Pereira <langerpereira12@gmail.com>
2024-07-22 22:53:46 +0530

    parallax effect

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
-const parallex_el = document.querySelectorAll(".parallex");
+const parallaxElements = document.querySelectorAll(".parallax");
 
 let xValue = 0, yValue = 0;
+let requestId;
 
-window.addEventListener("mousemove", (e) =>{
+window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
-    parallex_el.forEach((el) => {
-        el.style.transform = `translateX(calc(-50% + ${xValue}px)) translateY(calc(-50% + ${yValue}px))`;
-    });
-
+    if (!requestId) {
+        requestId = requestAnimationFrame(updateParallax);
+    }
 });
+
+function updateParallax() {
+    parallaxElements.forEach((el) => {
+        let speedx = parseFloat(el.dataset.speedx);
+        let speedy = parseFloat(el.dataset.speedy);
+        el.style.transform = `translate(${(-xValue * speedx)}px, ${(-yValue * speedy)}px)`;
+    });
+    requestId = null;  
+}
